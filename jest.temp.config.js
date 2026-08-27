bf8982597a377d19047359ee1b8a20b98508e520
ahmadogo <abdulkareemahmad413@gmail.com>
2026-08-27 09:03:44 +0100

    feat: implement landing page hero section

--- jest.temp.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  moduleNameMapper: {
-    "^@/(.*)$": "C:\\Users\\yunus\\Desktop\\coaches_frontend621\\$1",
-  },
-  testMatch: ["**/__tests__/ui/ResourceSearchBar.test.tsx"],
-  extensionsToTreatAsEsm: [],
-};
